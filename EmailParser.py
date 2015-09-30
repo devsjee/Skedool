184569d35f2efba976836e17271592e35e615dfb
devsjee <devsjee@gmail.com>
2015-10-01 02:29:08 +0530

    cross validation pending

--- EmailParser.py
+++ EmailParser.py
@@ -2,40 +2,58 @@ from nltk.tokenize import RegexpTokenizer
 
 
 class EmailParser:
-	def __init__(self,filename):
+	"""
+	The email to be parsed could be either passed through the filename
+	or given directly as text
+	i)	to read from file, the class takes the name of the file during instantiation, followed by 
+		the user call to the member function 'split_mail'
+	ii) 	to pass the mail content directly, call the 'tokenize_mail' member function
+	"""
+
+	def __init__(self,filename=''):
 		self.filename = filename
 
 	
 	def read_file(self):
+		"""
+			reads the file and returs a string containing the mailcontent
+		"""
 		with open(self.filename,'r') as f:
 			mailtext = f.readlines()
 			mailtext = ''.join(mailtext)
 			return mailtext
-			
-	def split_mail(self):
-
-		mailtext = self.read_file()
-		mails = self.tokenize_mail(mailtext)
-		self.write_mail(mails)
 
 
 	def tokenize_mail(self,mailtext):
+		"""
+		this function uses the RegexpTokeniser to split the mail text on the pattern defined
+		Each split is a separate mail. 
+		Returns a list of mails contained in the given mailtext
+		"""
+		
 		mails = []
 	
-		#splits entire mail into three parts : original mail, header of contained reply, reply mail
+		#splits entire mail into parts matching the 'On <Date Time> <person@mail.com> wrote:' pattern
 		tokenizer = RegexpTokenizer('\n[>|\s]*On[\s]* ([a-zA-Z0-9, :/<>@\.\"\[\]\r\n]*[\s]* wrote:)',gaps = True)
 		mail_indices = tokenizer.span_tokenize(mailtext)
 		
-		
+		#uses the splits' offset information from span_tokenize to split the actual mailcontent
+		#stores each split as an element of a list named 'mails'
 		start = end = 0
 		for index in mail_indices:
 			end = index[1]+1
 			mails.append(mailtext[start:end])
 			start = end
 
-		return mails
+		return mails	#list of the contained mails within a single mailtext
+
 
 	def write_mail(self,mails):
+		"""
+		creats a separate file for each of the contained mails
+		extracts the Date and Sender information from the individual contained mail
+		writes the info to the file 
+		"""
 		index = 0
 
 		for text in mails:
@@ -49,6 +67,19 @@ class EmailParser:
 	
 			index+=1
 
+			
+	def split_mail(self):
+		"""
+			this function consolidates the actions of the EmailParser class.
+			It reads the mail content from mail, tokenises it to separate mails
+			and writes the individual mails to separate files
+		"""
+		mailtext = self.read_file()
+		mails = self.tokenize_mail(mailtext)
+		self.write_mail(mails)
+
+
+	
 	
 			
 
--- ExtractFeatures.py
+++ ExtractFeatures.py
@@ -1,3 +1,145 @@
+import csv
+import nltk 
+from EmailParser import EmailParser
+from nltk.corpus import stopwords
+import random
 
-class ExtractFeatures:
+from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn.linear_model import LogisticRegression
+
+
+class learn_meeting_model:
+	"""
+	This class has member functions to read data from a 'csv' file, create feature sets out of it,
+	train a logistic regression classifier on the featuresets and print the cross validated results.
+	"""
+
+	def __init__(self,filename,level=1):
+		"""filename (for e.g., <location_data.csv> )is passed during instantiation of the class.
+		level is the number of self-contained mails that has to be considered for training
+		"""
+		self.filename = filename
+		data_table = self.read_csv()	#reads the datafile
+		featuresets = self.create_featuresets(data_table,level)	#creates feature sets
+		self.learn_model(featuresets)	#trains and tests a classifier
+		
+
+	def read_csv(self):		
+		"""
+		uses the csv package to read the data from csv file.
+		stores the data internally in the form of list of lists
+		"""
+		data_table = []
+		with open(self.filename,'rb') as f:
+			reader = csv.reader(f)
+			for row in reader:
+				data_table.append(row)
+			
+		return data_table
+
+
+
+	def extract_mail(self,data_table,level=1):
+		"""
+		this function processes the data_table row by row and for each row(i.e. each mail) extracts the contained mail
+		(upto the mentioned level) 
+
+		Returns two variables : cleaned_data and all_words
+
+		cleaned_data : a list of lists in the form [[(set of unique words in a mail),label of mail]]  --> extracts only the mailtext 
+		and label from the original data_table read from the csv file
+		
+		all_words : the list containing all words across all mails for the purpose of finding out the most frequent words
+		in the process of feature set creation
+		"""
+
+		cleaned_data = []
+		all_words = []
+
+		for row in data_table:
+			mailtext = row[-2]
+			category = row[-1]
+
+			mailParser = EmailParser()
+			mails= mailParser.tokenize_mail(mailtext)	#gets the contained mails within each mail
+			
+			row_words = set()
+
+			for i in range(level):	#level of nesting of replies to be considered for feature creation
+				mail_words = nltk.word_tokenize(mails[i].decode("utf8"))
+				for word in mail_words:
+					row_words.add(word)    #adding the words in current mail to a set to get unique tokens
+					all_words.append(word) #cumulating the words for frequency counting
+
+			cleaned_data.append([row_words,category]) #each item in cleaned data is [<set of words in mail>, label]
+
+		return all_words,cleaned_data
+
+
+	def remove_stopwords(self,all_words):
+		"""
+		Before using the most frequent words as features, it is processed to remove
+		the stop words taken from nltk.corpus
+		"""
+		stop_words = stopwords.words('english')
+		new_words = []
+		for word in all_words:
+			if word not in stop_words:
+				new_words.append(word)
+
+		return new_words
+
+
+	def find_features(self,words,word_features):
+		"""
+		this function creates a feature vector(in the form of a dict) for each mail
+		Each unique token in the mail is checked for it's presence in the word_features (most frequent words identified
+		as features)
+		"""
+		features = {}
+		
+		for w in word_features:
+			features[w] = (w in words)
+		
+		return features
+		
+
+	def create_featuresets(self,data_table,level=1):
+		"""
+		Forms the cleaned data out of orginal data_table
+		Creates 'all_words' which is a collection of all words across all mails in tha data_table
+		Removes stopwords from the 'all_words'
+		Identifies the word_features based on frequency
+		Creates and returns the featuresets for mails in the cleaned_data
+		"""
+
+		all_words,cleaned_data = self.extract_mail(data_table,level)
+		all_words = self.remove_stopwords(all_words)
+
+		all_words = nltk.FreqDist(all_words)
+		word_features = list(all_words.keys())[:2000]
+
+		featuresets = [(self.find_features(mail,word_features),label) for (mail,label) in cleaned_data]
+
+		return featuresets
+
+	def learn_model(self,featuresets):
+		"""
+		trains and tests the logistic regression classifier on the data
+		"""
+		random.shuffle(featuresets)
 	
+		limit = int(0.75*len(featuresets)) #partitioning 3:1 for train:test
+		train_set = featuresets[:limit]
+		test_set = featuresets[limit:]
+	
+		lr_classifier = SklearnClassifier(LogisticRegression())
+		lr_classifier.train(train_set)
+		
+		print 'Logistic classifier Accuracy : ',str(nltk.classify.accuracy(lr_classifier,test_set)*100)
+
+
+		#nb_classifier = nltk.NaiveBayesClassifier.train(train_set)
+		
+		#print 'Naive Bayes classifier Accuracy : ',str(nltk.classify.accuracy(nb_classifier,test_set)*100)
+		#nb_classifier.show_most_informative_features(30)
